c0b3a03b983c474532980143cc1f25d123f9e5da
PnthrLeo <leon-1402@mail.ru>
2020-10-31 18:57:10 +0300

    add greyscale filter

--- improc/_rgb2gray.py
+++ improc/_rgb2gray.py
@@ -1,1 +1,6 @@
-# file for black and white filter implementation
+def rgb2gray(image):
+    image = image/255.0
+    R = 0.2125*image[:, :, 0]
+    G = 0.7154*image[:, :, 1]
+    B = 0.0721*image[:, :, 2]
+    return R + G + B
